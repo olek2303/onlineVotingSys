5f3524920f025b4f090853c36b38b6bc681968dc
olek2303 <olek230303@gmail.com>
2023-02-14 19:32:34 +0100

    first commit

---
+++ src/main/java/servlets/NewServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NewServlet", value = "/newservlet")
+public class NewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("GET request in terminal");
+        response.setContentType("text/plain");
+        response.getWriter().println("Here is a line of text");
+        response.getWriter().println("And here another one");
+
+        PrintWriter out = response.getWriter();
+        String parameter1 = request.getParameter("parameter1");
+        String parameter2 = request.getParameter("parameter2");
+        String parameter3 = request.getParameter("parameter3");
+
+        out.println("To sa wczytane slowa: ");
+        out.println(parameter1);
+        out.println(parameter2);
+        out.println(parameter3);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("POST request in terminal");
+        PrintWriter out = response.getWriter();
+
+        out.println("Wczytanie 3 parametrow z zadania :");
+        out.println(request.getParameter("parameter1"));
+        out.println(request.getParameter("parameter2"));
+        out.println(request.getParameter("parameter3"));
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %></h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<a href="cool.html">Click Here</a>
+<br/>
+<a href="newservlet">Going to the new section</a>
+</body>
+</html>
